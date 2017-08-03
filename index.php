be1fbada6b05145061d71b2600e0a25f926354e7
sslghost <geraldjglasgow@gmail.com>
2017-08-02 22:46:25 -0400

    display for items

---
+++ index.php
@@ -0,0 +1,65 @@
+<div class="container">
+	<div class="row category-child" style="margin-top:20px">
+		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+                    <img class="img-responsive" src="http://kudo.co.id/wp-pictures/product/example/woven_che_white.jpg" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Kebutuhan Ibu dan Anak</span>
+                        </div>
+                    </div>
+                </a>
+        </div>
+		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/fashion.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Produk Pilihan</span>
+                        </div>
+                    </div>
+                </a>
+        </div>
+		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+                    <img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/hijab.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Produk Pilihan</span>
+                        </div>
+                    </div>
+                </a>
+        </div>
+		<div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/makanandaerah.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Produk Pilihan</span>
+                        </div>
+                    </div>
+                </a>
+        </div>
+        <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/tiket.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Produk Pilihan</span>
+                        </div>
+                    </div>
+                </a>
+        </div>
+        <div class="col-lg-2 col-md-4 col-xs-6 thumb ">
+                <a class="thumbnail" href="#">
+					<img class="img-responsive" src="http://image.kudo.co.id/wp-content/uploads/2015/10/holiday.png" alt="">
+                    <div class="wrapper">
+                        <div class="caption post-content">
+                            <span>Produk Pilihan</span>
+                        </div>
+                    </div>
+                </a>
+        </div>
+	</div>
+
+</div>
