450b3d35b0e6f82891ecde484bbe9195e6357842
sslghost <geraldjglasgow@gmail.com>
2017-08-02 23:41:26 -0400

    prototype webpage

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Heroic Features - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">HOME</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">cpu</a>
+                    </li>
+                    <li>
+                        <a href="#">ram</a>
+                    </li>
+                    <li>
+                        <a href="#">gpu</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+            <h1>PC Parts Database Prototype page</h1>
+        </header>
+
+        <hr>
+
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Featured Processors</h3>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Page Features -->
+        <div class="row text-center">
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/7700k.jpg" alt="7700k" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Core i7 7700k</h3>
+                        <p>4.2GHz Base Clock<br />
+                          4 Core 8 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                            <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/7600k.jpg" alt="7600k" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Core i5 7600k</h3>
+                        <p>3.8GHz Base Clock<br />
+                          4 Core 4 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                             <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/7100.jpg" alt="7100" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Core i3 7100</h3>
+                        <p>3.9GHz Base Clock<br />
+                          2 Core 4 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                             <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/G4620.jpg" alt="G4620" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Pentium G4620</h3>
+                        <p>3.7GHz Base Clock<br />
+                          2 Core 4 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                             <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <div class="row text-center">
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/7700k.jpg" alt="7700k" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Core i7 7700k</h3>
+                        <p>4.2GHz Base Clock<br />
+                          4 Core 8 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                            <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/7600k.jpg" alt="7600k" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Core i5 7600k</h3>
+                        <p>3.8GHz Base Clock<br />
+                          4 Core 4 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                             <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/7100.jpg" alt="7100" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Core i3 7100</h3>
+                        <p>3.9GHz Base Clock<br />
+                          2 Core 4 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                             <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="pics/G4620.jpg" alt="G4620" height="500" width="800">
+                    <div class="caption">
+                        <h3>Intel Pentium G4620</h3>
+                        <p>3.7GHz Base Clock<br />
+                          2 Core 4 Thread<br />
+                          LGA 1151 socket<br />
+                        </p>
+                        <p>
+                             <a href="#" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; PC Parts Database 2017</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
