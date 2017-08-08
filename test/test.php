9d8d3b076dab5121f7f934e6fd5db03576a51ca6
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-08-08 09:01:06 -0400

    - test

---
+++ test/test.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+
+<div id="demo-borders">
+    <div data-role="collapsible" data-inset="false">
+        <h3>Pets</h3>
+        <ul data-role="listview">
+            <li><a href="#">Canary</a></li>
+            <li><a href="#">Cat</a></li>
+            <li><a href="#">Dog</a></li>
+            <li><a href="#">Gerbil</a></li>
+            <li><a href="#">Iguana</a></li>
+            <li><a href="#">Mouse</a></li>
+        </ul>
+    </div><!-- /collapsible -->
+    <div data-role="collapsible" data-inset="false">
+        <h3>Farm animals</h3>
+        <ul data-role="listview">
+            <li><a href="#">Chicken</a></li>
+            <li><a href="#">Cow</a></li>
+            <li><a href="#">Duck</a></li>
+            <li><a href="#">Horse</a></li>
+            <li><a href="#">Pig</a></li>
+            <li><a href="#">Sheep</a></li>
+        </ul>
+    </div><!-- /collapsible -->
+</div>
+
+</body>
+</html>
