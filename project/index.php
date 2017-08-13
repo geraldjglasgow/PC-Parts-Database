c2688b05b49e8a8f5bad46c2f969864f4e1c1cf2
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-08-13 18:46:26 -0400

    - made featured items

--- project/index.php
+++ project/index.php
@@ -20,7 +20,8 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
   <!-- Database Connection -->
-  <?php include('./SQLFILES/databaseconnect.php'); ?>
+  <?php include('./SQLFILES/databaseconnect.php');
+  $conn = get_connection(); ?>
 
 </head>
 
@@ -41,10 +42,71 @@
         </div>
       </div>
       <!-- end sidebar -->
+      <?php
+      $q = "SELECT name, brand, series, speed, core, thread, socket, price FROM cpu WHERE gen LIKE '%X' AND name <> '7900X'";
+      $query = $conn->prepare($q);
+      $query = execQuery($query, $conn);
+      $result = $query->fetchAll();
+       ?>
 
-      <div class="col-md-9">
+      <div class="col-md-9"><div class = "lead"> Featured Items </div>
         <div class="row">
 
+
+           <?php
+          //  CPU
+           $j=0;
+           while($j<sizeof($result)){
+             echo '
+             <div class="col-sm-5 col-lg-3 col-md-4 text-center">
+             <div class="thumbnail">
+             <img src="./pictures/cpu/'.$result[$j][0].'.jpg" alt="'.$result[$j][0].'">
+             <div class="caption">
+             <h4>'.$result[$j][1].' '.$result[$j][2].' '.$result[$j][0].'</h4>
+             <p">'.$result[$j][3].'GHz Base Clock<br />
+             '.$result[$j][4].' Core '.$result[$j][5].' Thread<br />
+             '.$result[$j][6].' socket<br />
+             $'.$result[$j][7].'<br />
+             </p>
+             <a href="./moreinfo.php?cname='.$result[$j][0].'">More Info</a>
+             </div>
+             </div>
+             </div>
+             ';
+             $j +=1;
+            }
+              // GPU
+              $q = "SELECT gpu, brand, coreClock, price, name, memory FROM gpumem NATURAL JOIN videoCard WHERE name LIKE '%ti%' AND coreClock > 1520";
+              $query = $conn->prepare($q);
+              try {
+                $conn->beginTransaction();
+                $query->execute();
+                $conn->commit();
+              } catch (Exception $e) {
+                $db->rollback();
+              }
+              $result = $query->fetchAll();
+             $j=0;
+             while($j<sizeof($result)){
+               echo '
+               <div class="col-sm-5 col-lg-3 col-md-4 text-center">
+               <div class="thumbnail">
+               <img src="./pictures/gpu/'.$result[$j][4].'.jpg" alt="'.$result[$j][4].'">
+               <div class="caption">
+               <h4>'.$result[$j][1].' '.$result[$j][0].'</h4>
+               <p">$'.$result[$j][3].'<br />
+               '.$result[$j][2].' MHz Core Clock<br />
+               '.$result[$j][5].'GB Video Memory<br />
+               </p>
+               <a href="./moreinfo.php?cname='.$result[$j][4].'">More Info</a>
+               </div>
+               </div>
+               </div>
+               ';
+               $j +=1;
+             }
+
+           ?>
         </div>
       </div>
 
